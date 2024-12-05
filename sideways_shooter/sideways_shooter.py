d16d02ca7d8241bfb087a1ad51c783c079a6bae3
wylie102 <wylie102@gmail.com>
2024-12-05 18:18:24 +0000

    File Organisation

--- sideways_shooter/sideways_shooter.py
+++ sideways_shooter/sideways_shooter.py
@@ -48,6 +48,10 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
         elif event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
             sys.exit()
         elif event.key == pygame.K_SPACE:
