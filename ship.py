dec65e61bc7406e6989667938f8d541932d376e3
wylie102 <wylie102@gmail.com>
2024-12-05 13:00:21 +0000

    Ship can move left and right. Sip speed moved to settings.

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialise the ship and set it's starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get it's rect.
@@ -15,13 +16,22 @@ class Ship:
         # Start each new ship at the bottom centre of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #Store a float for the ship's exact horizontal position.
+        self.x = float(self.rect.x)
+
         # Movement flag; start with a ship that's not moving
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
-        """Update the ship's position based on the movement flag."""
+        """Update the ship's position based on movement flags."""
         if self.moving_right:
-            self.rect.x +=1
+            self.x += self.settings.ship_speed
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+        
+        # Update rect object from self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at it's current location."""
