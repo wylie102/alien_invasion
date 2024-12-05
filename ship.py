307e0e45455d62d65b1f7ed463e257024b27d252
wylie102 <wylie102@gmail.com>
2024-12-05 17:07:07 +0000

    Ship can move in all directions. Added alternate rocket_game file. Can quit with escape.

--- ship.py
+++ ship.py
@@ -18,10 +18,13 @@ class Ship:
 
         #Store a float for the ship's exact horizontal position.
         self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
 
         # Movement flag; start with a ship that's not moving
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
         """Update the ship's position based on movement flags."""
@@ -29,9 +32,14 @@ class Ship:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
         
         # Update rect object from self.x.
         self.rect.x = self.x
+        self.rect.y = self.y
 
     def blitme(self):
         """Draw the ship at it's current location."""
