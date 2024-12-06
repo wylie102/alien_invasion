53f915e3aa9069cce4514978c03e8d00bd6884cf
wylie102 <wylie102@gmail.com>
2024-12-06 16:49:07 +0000

    Aliens Move.

--- alien.py
+++ alien.py
@@ -8,6 +8,7 @@ class Alien(Sprite):
         """Initialise the alien and set it's starting position."""
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         # Load the alien image and set it's rect attribute.
         self.image = pygame.image.load('images/alien.bmp')
@@ -19,3 +20,13 @@ class Alien(Sprite):
 
         # Store the alien's exact horizontal position.
         self.x = float(self.rect.x)
+    
+    def check_edges(self):
+        """Return True if the alien is at the edge of the screen."""
+        screen_rect = self.screen.get_rect()
+        return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
+
+    def update(self):
+        """Move the alien right or left."""
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
+        self.rect.x = self.x
